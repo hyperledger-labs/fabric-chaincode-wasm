6d97f72535f1ba4bd5fc7566cc5f1f45707fbf82
shubham aggarwal <shubham6300@gmail.com>
2019-09-05 21:04:47 +0800

    Merge branches
    Signed-off-by: shubham aggarwal <shubham6300@gmail.com>

---
+++ tools/file-encoder/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"github.com/atotto/clipboard"
+)
+
+func main() {
+
+
+	fmt.Println("Enter file path : ")
+	var filepath string
+	fmt.Scanf("%s", &filepath)
+
+	fmt.Println("Trying to read file : "+filepath)
+
+	file, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		panic(err)
+	}
+
+	encodedFile := hex.EncodeToString(file)
+	clipboard.WriteAll(encodedFile)
+	fmt.Printf("Copied to clipboard. Encoded file: "+encodedFile)
+}
