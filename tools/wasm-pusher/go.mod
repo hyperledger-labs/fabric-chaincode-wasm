faeab086957b83565fcc917b03a12ed2d3f0d322
Shubham <4557571+kleash@users.noreply.github.com>
2019-09-19 11:41:14 +0800

    Wasm pusher test (#16)
    Add cli flags and env config to wasmpusher
    
    - Instead of scanning user input from terminal for every new parameter, use cli flags and env config
    - uses viper and cobra library
    - use existing dep Gopkg.toml to populate go.mod
    
    Signed-off-by: Morgan Bauer <mbauer@us.ibm.com>
    Signed-off-by: shubham aggarwal <shubham6300@gmail.com>

---
+++ tools/wasm-pusher/go.mod
@@ -0,0 +1,33 @@
+module github.com/kleash/fabric-chaincode-wasm/tools/wasm-pusher
+
+go 1.13
+
+require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cloudflare/cfssl v0.0.0-20180223231731-4e2dcbde5004 // indirect
+	github.com/go-kit/kit v0.8.0 // indirect
+	github.com/go-logfmt/logfmt v0.4.0 // indirect
+	github.com/golang/mock v1.3.2-0.20190713102442-dd8d2a22370e // indirect
+	github.com/google/certificate-transparency-go v1.0.10-0.20180222191210-5ab67e519c93 // indirect
+	github.com/hyperledger/fabric-lib-go v1.0.0 // indirect
+	github.com/hyperledger/fabric-sdk-go v1.0.0-alpha5
+	github.com/magiconair/properties v1.8.1 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
+	github.com/pelletier/go-toml v1.4.0 // indirect
+	github.com/pkg/errors v0.8.1
+	github.com/prometheus/client_golang v0.8.0 // indirect
+	github.com/prometheus/client_model v0.0.0-20190812154241-14fe0d1b01d4 // indirect
+	github.com/prometheus/common v0.0.0-20180518154759-7600349dcfe1 // indirect
+	github.com/prometheus/procfs v0.0.5 // indirect
+	github.com/spf13/afero v1.2.2 // indirect
+	github.com/spf13/cobra v0.0.5
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
+	github.com/spf13/viper v1.3.2
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/crypto v0.0.0-20190911031432-227b76d455e7 // indirect
+	golang.org/x/sys v0.0.0-20190916202348-b4ddaad3f8a3 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	google.golang.org/genproto v0.0.0-20190916214212-f660b8655731 // indirect
+	google.golang.org/grpc v1.23.1 // indirect
+)
--- tools/wasm-pusher/main.go
+++ tools/wasm-pusher/main.go
@@ -7,9 +7,10 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
-	"bufio"
 	"encoding/hex"
 	"fmt"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"os"
 
@@ -20,18 +21,25 @@ import (
 	"github.com/pkg/errors"
 )
 
+var cfgFile, user, org, channelName, wasmfile, chaincodename string
+var args []string
+
 func main() {
-	sdk, err := fabsdk.New(config.FromFile("./first-network.yaml"))
+
+	chainCodeID := "wasmcc"
+
+	err := readConfigVar()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sdk, err := fabsdk.New(config.FromFile(cfgFile))
 	if err != nil {
 		fmt.Println(errors.WithMessage(err, "failed to create SDK"))
 		os.Exit(-1)
 	}
 	defer sdk.Close()
-
-	user := "User1"
-	org := "Org1"
-	channelName := "mychannel"
-	chainCodeID := "wasmcc"
 
 	clientChannelContext := sdk.ChannelContext(channelName, fabsdk.WithUser(user), fabsdk.WithOrg(org))
 	// client for interacting directly with the ledger
@@ -55,13 +63,9 @@ func main() {
 		os.Exit(-1)
 	}
 
-	fmt.Println("Enter wasm file path : ")
-	var filepath string
-	fmt.Scanf("%s", &filepath)
+	fmt.Println("Trying to read file : " + wasmfile)
 
-	fmt.Println("Trying to read file : " + filepath)
-
-	file, err := ioutil.ReadFile(filepath)
+	file, err := ioutil.ReadFile(wasmfile)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(-1)
@@ -71,34 +75,23 @@ func main() {
 	encodedFile := hex.EncodeToString(file)
 	fmt.Printf("encoded file length : %d \n", len(encodedFile))
 
-	//Get chaincode name from user
-	fmt.Println("Please enter wasm chaincode name : ")
-	var wasmChaincodeName string
-	fmt.Scanf("%s", &wasmChaincodeName)
+	var txnargs [][]byte
 
-	//Get init parameters from user
-	fmt.Println("Please enter all parameters for your init function separated by new line. Hit enter twice for exit: ")
+	//Add wasm chaincode name to arguments
+	txnargs = append(txnargs, []byte(chaincodename))
 
-	var args [][]byte
-
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		input := scanner.Text()
-
-		if input == ""{
-			break
+	if args != nil {
+		for _, arg := range args {
+			txnargs = append(txnargs, []byte(arg))
 		}
-
-		args = append(args, []byte(input))
 	}
-
 
 	fmt.Println("Executing transaction")
 
 	response, err := client.Execute(channel.Request{
 		ChaincodeID: chainCodeID,
 		Fcn:         "create",
-		Args: args,
+		Args:        txnargs,
 	})
 
 	if err != nil {
@@ -106,4 +99,72 @@ func main() {
 		os.Exit(-1)
 	}
 	fmt.Println(response)
+}
+
+func readConfigVar() error {
+	var cmd = &cobra.Command{
+		Use:   "wasm-pusher",
+		Short: "WASM pusher is a tool used to install webassembly chaincodes in Hyperledger fabric using fabric-chaincode-wasm",
+		Long: `This tool is based on fabric-go-sdk. It can be used to directly install a wasm chaincode on Hyperledger fabric.
+WASMCC supports wasm chaincode in three formats i.e. .wasm binary, zip file containing wasm binary and hex encoded wasm file.
+WASM pusher can install wasm chaincode in any of the above formats.`,
+		RunE: func(command *cobra.Command, args []string) error {
+
+			//validate all config parameters
+			cfgFile = viper.GetString("configfile")
+			if cfgFile == "" {
+				return fmt.Errorf("configfile flag is required")
+			}
+			user = viper.GetString("user")
+			if user == "" {
+				return fmt.Errorf("user flag is required")
+			}
+			org = viper.GetString("org")
+			if org == "" {
+				return fmt.Errorf("org flag is required")
+			}
+			channelName = viper.GetString("channelName")
+			if channelName == "" {
+				return fmt.Errorf("channelName flag is required")
+			}
+			wasmfile = viper.GetString("wasmfile")
+			if wasmfile == "" {
+				return fmt.Errorf("wasmfile flag is required")
+			}
+			chaincodename = viper.GetString("chaincodename")
+			if chaincodename == "" {
+				return fmt.Errorf("chaincodename flag is required")
+			}
+			args = viper.GetStringSlice("args")
+			return nil
+		},
+	}
+
+	//Read from environment variable and cli flag
+	viper.AutomaticEnv()
+	flags := cmd.Flags()
+
+	//Default value of config file is ./first-network.yaml
+	flags.StringVarP(&cfgFile, "configfile", "c", "./first-network.yaml", "fabric config file")
+	viper.BindPFlag("configfile", flags.Lookup("configfile"))
+
+	//Default value of user is User1
+	flags.StringVarP(&user, "user", "u", "User1", "User identity to use")
+	viper.BindPFlag("user", flags.Lookup("user"))
+
+	//Default value of organisation name is Org1
+	flags.StringVarP(&org, "org", "o", "Org1", "Organization of user")
+	viper.BindPFlag("org", flags.Lookup("org"))
+
+	//Default value of channel name is mychannel
+	flags.StringVarP(&channelName, "channelName", "C", "mychannel", "channel on which wasmcc is installed")
+	viper.BindPFlag("channelName", flags.Lookup("channelName"))
+	flags.StringVarP(&user, "wasmfile", "w", "", "wasm chaincode filepath")
+	viper.BindPFlag("wasmfile", flags.Lookup("wasmfile"))
+	flags.StringVarP(&chaincodename, "chaincodename", "n", "", "wasm chaincode name")
+	viper.BindPFlag("chaincodename", flags.Lookup("chaincodename"))
+	flags.StringSliceVarP(&args, "args", "a", nil, "arguments for init fn of wasm chaincode")
+	viper.BindPFlag("args", flags.Lookup("args"))
+
+	return cmd.Execute()
 }
